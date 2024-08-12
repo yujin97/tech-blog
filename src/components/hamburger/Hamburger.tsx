ea9c9f68872f187c611c969b298cdf2913e61607
Eugene <eugene97@connect.hku.hk>
2024-08-12 23:49:39 +0900

    refactor: preserve reactivity by creating function

--- src/components/hamburger/Hamburger.tsx
+++ src/components/hamburger/Hamburger.tsx
@@ -1,5 +1,4 @@
 import type { JSX } from "solid-js";
-import { splitProps } from "solid-js";
 import { HamburgerLine } from "./HamburgerLine";
 
 interface Props {
@@ -8,15 +7,15 @@ interface Props {
 }
 
 export function Hamburger(props: Props) {
-  const [reactive, others] = splitProps(props, ["isOpen"]);
+  const isOpen = () => props.isOpen;
 
   return (
     <>
-      <div class="fixed lg:hidden" onClick={others.onClick}>
+      <div class="fixed lg:hidden" onClick={props.onClick}>
         <div class="h-6 w-9 flex flex-col justify-between">
-          <HamburgerLine isOpen={reactive.isOpen} lineNumber={1} />
-          <HamburgerLine isOpen={reactive.isOpen} lineNumber={2} />
-          <HamburgerLine isOpen={reactive.isOpen} lineNumber={3} />
+          <HamburgerLine isOpen={isOpen()} lineNumber={1} />
+          <HamburgerLine isOpen={isOpen()} lineNumber={2} />
+          <HamburgerLine isOpen={isOpen()} lineNumber={3} />
         </div>
       </div>
     </>
